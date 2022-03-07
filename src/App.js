d9077e02a6ae5a7d98d59306ca127c6c238b729a
bipin-01 <poudel7700@gmail.com>
2022-03-07 12:14:27 +0545

    added toggle feature

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Cart from './components/Cart/Cart';
+import { useSelector } from "react-redux";
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+
+function App() {
+
+  const showCart = useSelector(state => state.ui.cartIsVisible)
+  return (
+    <Layout>
+     {showCart && <Cart /> }
+      {/* <Cart /> */}
+      <Products />
+    </Layout>
+  );
+}
+
+export default App;
